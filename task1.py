76cde67b7e0f77192e69034b4e446e6ad7e438c3
Aleksa <bigovicaleksa@gmail.com>
2018-12-13 11:17:03 +0100

    Projekat

--- task1.py
+++ task1.py
@@ -1,6 +1,7 @@
 import time
 from task2 import LinkedQueue
 from task3 import quick_sort
+import random
 
 def merge(S1, S2, S):
 
@@ -35,10 +36,25 @@ def merge_sort(S):
 
 
 n = LinkedQueue()
-n.enqueue(3)
-n.enqueue(5)
-n.enqueue(1)
-n.enqueue(2)
+m = LinkedQueue()
+p =  LinkedQueue()
+
+for i in range(1000):
+    n.enqueue(random.randint(0,10000))
+    m.enqueue(random.randint(0, 10000))
+    p.enqueue(random.randint(0, 10000))
+
+
+for i in range(1000):
+    n.enqueue(random.randint(0,100000))
+    m.enqueue(random.randint(0, 100000))
+    p.enqueue(random.randint(0, 100000))
+
+
+for i in range(1000):
+    n.enqueue(random.randint(0,1000000))
+    m.enqueue(random.randint(0, 1000000))
+    p.enqueue(random.randint(0, 1000000))
 
 
 print("Queue: ", n.print_queue())
@@ -46,22 +62,94 @@ start1 = time.clock()
 merge_sort(n)
 stop1 = time.clock()
 print("Sorted queue: ", n.print_queue())
+print(stop1-start1)
+
+t1=stop1-start1
+
+
+print("")
+print("Queue: ", m.print_queue())
+start1 = time.clock()
+merge_sort(m)
+stop1 = time.clock()
+print("Sorted queue: ", m.print_queue())
+print(stop1-start1)
+
+t2=stop1-start1
+
+print('')
+
+print("Queue: ", p.print_queue())
+start1 = time.clock()
+merge_sort(p)
+stop1 = time.clock()
+print("Sorted queue: ", p.print_queue())
+print(stop1-start1)
+
+t3=stop1-start1
 
 print("")
 
 
-m = LinkedQueue()
-m.enqueue(3)
-m.enqueue(5)
-m.enqueue(1)
-m.enqueue(2)
 
-print("Queue: ", m.print_queue())
-start2 = time.clock()
-quick_sort(m)
-stop2 = time.clock()
-print("Sorted queue: ", m.print_queue())
+n1 = LinkedQueue()
+m1 = LinkedQueue()
+p1 =  LinkedQueue()
+
+for i in range(1000):
+    n1.enqueue(random.randint(0,10000))
+    m1.enqueue(random.randint(0, 10000))
+    p1.enqueue(random.randint(0, 10000))
 
 
-print(stop1-start1, stop2-start2)
+for i in range(1000):
+    n1.enqueue(random.randint(0,100000))
+    m1.enqueue(random.randint(0, 100000))
+    p1.enqueue(random.randint(0, 100000))
+
+
+for i in range(1000):
+    n1.enqueue(random.randint(0,1000000))
+    m1.enqueue(random.randint(0, 1000000))
+    p1.enqueue(random.randint(0, 1000000))
+
+
+print("Queue: ", n1.print_queue())
+start1 = time.clock()
+quick_sort(n1)
+stop1 = time.clock()
+print("Sorted queue: ", n1.print_queue())
+print(stop1-start1)
+
+t11=stop1-start1
+
+
+
+print("")
+print("Queue: ", m1.print_queue())
+start1 = time.clock()
+quick_sort(m1)
+stop1 = time.clock()
+print("Sorted queue: ", m1.print_queue())
+print(stop1-start1)
+
+t22=stop1-start1
+
+
+print('')
+
+print("Queue: ", p1.print_queue())
+start1 = time.clock()
+quick_sort(p1)
+stop1 = time.clock()
+print("Sorted queue: ", p1.print_queue())
+print(stop1-start1)
+
+t33=stop1-start1
+
+print(" ")
+
+print(t1-t11,t2-t22,t3-t33)
+
+
 
